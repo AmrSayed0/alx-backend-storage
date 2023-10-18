065456dddf94697f03d2a7408de2a32ebd2b2315
AmrSayed0 <amracademycode@gmail.com>
2023-10-18 18:49:40 +0300

    Fixing errors and issues

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,29 +4,41 @@ from typing import Callable
 from functools import wraps
 import redis
 import requests
-redis_client = redis.Redis()
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def url_count(method: Callable) -> Callable:
     """Counts how many times a URL is called"""
+
     @wraps(method)
     def wrapper(*args, **kwargs):
         url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(f'{url}')
-        if cached:
-            return cached.decode('utf-8')
-        redis_client.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
+        redis_key = f'count:{url}'
+        redis_value = redis_client.get(redis_key)
+
+        if redis_value:
+            return redis_value.decode('utf-8')
+
+        response = method(url)
+        redis_client.incr(redis_key)
+        redis_client.setex(redis_key, 10, response)
+
+        return response
+
     return wrapper
 
 
 @url_count
 def get_page(url: str) -> str:
     """Gets the HTML content of a web page"""
-    response = requests.get(url)
-    return response.text
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception for bad requests
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return f"Error: {e}"
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk'))
